dd20d883fa91910b4caea88ddddd86ed551d76c2
aaron-zhao15 <aaron.zhao15@gmail.com>
2019-03-04 21:52:41 -0800

    finish things

--- src/Elevens/Card.java
+++ src/Elevens/Card.java
@@ -10,20 +10,45 @@ public class Card
 			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
 
 	//instance variables
-		//String suit
-		//int face
+	private String suit;
+        private int face;
 
   	//constructors
-
+        public Card(String s, int f){
+            this.suit = s;
+            this.face = f;
+        }
+        public Card(String s){
+            this.suit = s;
+        }
+        public Card(int f){
+            this.face = f;
+        }
+        
+        
 
 	// modifiers
 		//set methods
+        public void setSuit(String s){
+            this.suit = s;
+        }
+        public void setFace(int f){
+            this.face = f;
+        }
 
 
   	//accessors
 		//get methods
-
+        public String getSuit(){
+            return suit;
+        }
+        public int getFace(){
+            return face;
+        }
 
   	//toString
+        public String toString(){
+            return FACES[face] + " of " + suit;
+        }
 
  }
